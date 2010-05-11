ce22e0791c17ab0ccba99777a3282dc9757b869b
Jeremy Grosser <synack@digg.com>
2010-05-11 06:39:53 -0700

    Implement my own damn caching

--- digg/api.py
+++ digg/api.py
@@ -6,6 +6,8 @@ http://github.com/sixohsix/twitter/
 from exceptions import Exception
 from urllib import urlencode
 import urllib2
+from rfc822 import parsedate
+from time import time, mktime
 
 try:
     import json
@@ -22,21 +24,30 @@ class DiggError(Exception):
     pass
 
 class DiggCall(object):
-    def __init__(self, endpoint, methodname='', user_agent=None):
+    def __init__(self, endpoint, methodname='', user_agent=None, cache=None):
         self.endpoint = endpoint
         self.methodname = methodname
         self.user_agent = user_agent
+        self.cache = cache
 
     def __getattr__(self, k):
         try:
             return object.__getattr__(self, k)
         except AttributeError:
-            return DiggCall(self.endpoint, '.'.join((self.methodname, k)).lstrip('.'))
+            return DiggCall(self.endpoint, '.'.join((self.methodname, k)).lstrip('.'), cache=self.cache)
 
     def __call__(self, **kwargs):
         kwargs['method'] = self.methodname
         kwargs['type'] = 'json'
         kwargs = urlencode(kwargs)
+
+        if self.cache:
+            if kwargs in self.cache:
+                expires, response = self.cache[kwargs]
+                if expires < time():
+                    return response
+                else:
+                    del self.cache[kwargs]
 
         if self.methodname in POST_ACTIONS:
             # HTTP POST
@@ -51,12 +62,47 @@ class DiggCall(object):
         
         try:
             handle = urllib2.urlopen(req)
-            return json.loads(handle.read())
+            response = json.loads(handle.read())
+            if self.cache != None:
+                self.update_cache(key=kwargs, value=response, headers=handle.info())
+            return response
         except urllib2.HTTPError, e:
             raise DiggError('Digg sent status %i for method: %s\ndetails: %s' % (e.code, self.methodname, e.fp.read()))
 
+    def update_cache(self, key, value, headers):
+        cache = True
+        expires = int(time()) + 3600
+
+        expires = headers.get('Expires', None)
+        if expires:
+            expires = mktime(parsedate(expires))
+        else:
+            expires = int(time()) + 3600
+
+        cache_control = headers.get('Cache-Control', '')
+        for control in cache_control.split(','):
+            control = control.strip(' ')
+            control = control.split('=')
+            if len(control) == 2:
+                k, v = control
+            else:
+                k = control
+                v = None
+
+            if k in ('private', 'no-cache', 'no-store', 'must-revalidate'):
+                cache = False
+
+            if k in ('max-age', 'min-fresh'):
+                try:
+                    expires = int(time()) + int(v)
+                except ValueError:
+                    pass
+
+        if cache:
+            self.cache[key] = (expires, value)
+
 class Digg(DiggCall):
-    def __init__(self, endpoint='http://services.digg.com/1.0/endpoint', user_agent='python-digg/0.1'):
-        DiggCall.__init__(self, endpoint=endpoint, user_agent=user_agent)
+    def __init__(self, endpoint='http://services.digg.com/1.0/endpoint', user_agent='python-digg/0.1', cache=None):
+        DiggCall.__init__(self, endpoint=endpoint, user_agent=user_agent, cache=cache)
 
 __all__ = ['Digg']
--- test.py
+++ test.py
@@ -1,9 +1,10 @@
 from digg.api import Digg
 from pprint import pprint
 
-api = Digg()
+cache = {}
+api = Digg(cache=cache)
 
-for story in api.story.getHot()['stories']:
+for story in api.story.getHot()['stories'][:5]:
     print story['title']
     print '%i diggs, %i comments' % (story['diggs'], story['comments'])
     print 'Dugg by', ', '.join([x['user'] for x in api.story.getDiggs(story_id=story['id'])['diggs']]), '\n'
