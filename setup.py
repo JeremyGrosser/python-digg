fe4ff89f4a61fb33766bba27a7fc832bad2ba681
Jeremy Grosser <synack@digg.com>
2010-07-27 15:25:23 -0700

    Version bump

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ except ImportError:
     from distutils.core import setup
 
 setup(name='python-digg',
-    version='1.1',
+    version='1.2',
     description='Python interface to the Digg API',
     author='Jeremy Grosser',
     author_email='synack@digg.com',
